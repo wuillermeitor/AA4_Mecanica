9b69e0680f24f4376184fb84fc613e1338bbd7e9
Guillermo Sánchez <guillermosanchezsanchez@enti.cat>
2018-04-11 13:25:02 +0200

    kek

--- code/src/physics.cpp
+++ code/src/physics.cpp
@@ -2,8 +2,37 @@
 #include <imgui\imgui_impl_sdl_gl3.h>
 #include <glm/glm.hpp>
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <glm/gtc/quaternion.hpp>
+
+#define GRAVEDAD -9.81f
+float impulsoInicial = 250.f;
 
 using v3 = glm::vec3;
+using m4 = glm::mat4;
+
+namespace Cube {
+	extern void setupCube();
+	extern void cleanupCube();
+	extern void updateCube(const glm::mat4& transform);
+	extern void drawCube();
+
+	m4 matrix;
+
+	v3 pos;
+	m4 orientation;
+	v3 vel;
+	v3 linM;
+	v3 angM;
+	v3 sumF;
+	float mass;
+}
+
+namespace UV {
+	float currentTime=0;
+}
+
 float eCo=0.5;
 float fCo=0.5;
 #define NPARTICLES 1
@@ -35,9 +64,6 @@ const glm::vec3 planePoint[6]{
 	{ 0, 5, 5 }
 };
 
-float arrayParticles[NPARTICLES * 3];
-particle arrayStructParticles[NPARTICLES];
-
 //COLISIONES PLANO
 float planeD(glm::vec3 normal, glm::vec3 puntoP) {
 	return -glm::dot(normal, puntoP);
@@ -63,8 +89,15 @@ void rebote(particle &particula, glm::vec3 normal, glm::vec3 planeSpot) {
 	particula.V -= fCo*velocidadTangencial;
 }
 
+float getRandBetweenFloats(float x, float y) {
+	float random = ((float)rand()) / (float)RAND_MAX;
+	float diff = y - x;
+	float r = random * diff;
+	return x + r;
+}
 
 bool show_test_window = false;
+
 void GUI() {
 	bool show = true;
 	ImGui::Begin("Physics Parameters", &show, 0);
@@ -85,17 +118,56 @@ void GUI() {
 	}
 }
 
+void myUpdateCube(float dt){
+	Cube::sumF = { 0, GRAVEDAD + impulsoInicial, 0 };
+
+	Cube::linM += dt*Cube::sumF;
+
+	Cube::vel = Cube::linM / Cube::mass;
+	Cube::pos = Cube::pos + Cube::vel * dt;	
+
+	Cube::matrix={	1, 0, 0, 0,
+					0, 1, 0, 0,
+					0, 0, 1, 0,
+					Cube::pos.x, Cube::pos.y, Cube::pos.z, 1 };
+	impulsoInicial = 0;
+}
+
 void PhysicsInit() {
-	// Do your initialization code here...
-	// ...................................
+	srand(time(NULL));
+	Cube::mass = 1.f;
+
+	Cube::sumF = { 0, -9.81+10, 0 };
+
+	Cube::linM = { 0, 0, 0 };
+	Cube::angM = { 0, 0, 0 };
+
+	Cube::vel = { 0, 0, 0 };
+	Cube::pos = { getRandBetweenFloats(-5, 5), getRandBetweenFloats(0, 10), getRandBetweenFloats(-5, 5) };
+
+	Cube::orientation = {	1, 0, 0, 0,
+							0, 1, 0, 0,
+							0, 0, 1, 0,
+							0, 0, 0, 1	};
+	//Cube::vel = { 0, 0, 0 };
+	//Cube::velO = { 0, 0, 0 };
+
+	Cube::matrix = {1, 0, 0, 0,
+					0, 1, 0, 0,
+					0, 0, 1, 0,
+					Cube::pos.x, Cube::pos.y, Cube::pos.z, 1 };
+
+	Cube::setupCube();
 }
 
 void PhysicsUpdate(float dt) {
-	// Do your update code here...
-	// ...........................
+	myUpdateCube(dt);
+	Cube::updateCube(Cube::matrix);
+	Cube::drawCube();
+	
+	UV::currentTime += dt;
 }
 
 void PhysicsCleanup() {
-	// Do your cleanup code here...
-	// ............................
+	Cube::cleanupCube();
 }
